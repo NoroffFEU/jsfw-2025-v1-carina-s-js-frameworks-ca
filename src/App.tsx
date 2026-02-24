deab3a6a147d21560ff8417349220cafb072bb4f
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-02-24 11:59:59 +0100

    Testing Iconify plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-red-700">JavaScript Frameworks CA</h1>
+      <h1 className="text-primary-dark">JavaScript Frameworks CA</h1>
       <p className="font-light">This is a test</p>
-      <p>
-        <span className="icon-[mdi-light--home]"></span>
+      <p className="text-secondary-dark">
+        <span className="iconify-[material-symbols--home-outline]"></span>
       </p>
     </>
   );
