f3ba671398cd2d59283fef1180cd7f39c05ab3d4
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-02-24 10:25:51 +0100

    Test husky pre-commit hook 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-red-700">JavaScript Frameworks CA</h1>
-      <p className="font-bold">This is a test</p>
+      <p className="font-light">This is a test</p>
     </>
   );
 }
