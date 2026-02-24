e5740ee767086dd57269340169ddbe8f63a9d1ba
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-02-24 11:47:04 +0100

    Added google font, iconify plugin and colours to styles.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ function App() {
     <>
       <h1 className="text-red-700">JavaScript Frameworks CA</h1>
       <p className="font-light">This is a test</p>
+      <p>
+        <span className="icon-[mdi-light--home]"></span>
+      </p>
     </>
   );
 }
