287c12381633c776fe1d90efdbbdea9ee0590841
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-02-28 12:15:28 +0100

    Added aria labels to cart and favourites buttons

--- src/components/cart/CartButton.tsx
+++ src/components/cart/CartButton.tsx
@@ -7,6 +7,7 @@ const CartButton = ({ onClick }: CartButtonProps) => {
     <button
       onClick={onClick}
       className="sm:hover:bg-secondary-ultra-light flex size-8 items-center justify-center rounded-full bg-white text-lg font-medium transition duration-300 ease-in-out sm:size-12"
+      aria-label="Cart"
     >
       <span className="iconify-[material-symbols--shopping-bag-outline]"></span>
     </button>
--- src/components/favourites/FavouriteButton.tsx
+++ src/components/favourites/FavouriteButton.tsx
@@ -7,6 +7,7 @@ const FavouriteButton = ({ onClick }: FavouriteButtonProps) => {
     <button
       onClick={onClick}
       className="sm:hover:bg-secondary-ultra-light flex size-8 items-center justify-center rounded-full bg-white text-lg font-medium transition duration-300 ease-in-out sm:size-12"
+      aria-label="Favourites"
     >
       <span className="iconify-[material-symbols--favorite-outline]"></span>
     </button>
