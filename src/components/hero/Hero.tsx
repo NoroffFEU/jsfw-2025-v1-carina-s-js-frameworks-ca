0ff6ddcc985a7bfc8432994350b44c77eeb373cd
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-02-28 21:02:50 +0100

    Created hero component

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,38 @@
+import HeroImageMobile from "../../assets/images/hero-image-750.webp";
+import HeroImageDesktop from "../../assets/images/hero-image-2000.webp";
+
+const Hero = () => {
+  return (
+    <section className="relative h-100 w-full overflow-hidden bg-cover md:h-137">
+      <picture>
+        <source media="(min-width: 768px)" srcSet={HeroImageDesktop} />
+        <img
+          src={HeroImageMobile}
+          alt="Young woman with fashionable clothing and accessories in front of a pink wall."
+          className="absolute inset-0 h-full w-full object-cover sm:rounded"
+        />
+      </picture>
+      <div className="relative flex h-full w-[50%] flex-col justify-center gap-4 px-4 text-white md:px-12">
+        <div>
+          <p className="text-xl font-light uppercase md:text-3xl">New Season</p>
+          <h1 className="text-5xl font-black uppercase md:text-[96px]">
+            Favourites
+          </h1>
+        </div>
+        <p className="text-lg font-light md:text-2xl">
+          Discover this season’s favourites from top brands all across the
+          world.
+        </p>
+        <a
+          href="#products-section"
+          className="group flex items-center gap-2 text-lg font-semibold transition duration-300 ease-in-out hover:font-bold md:text-2xl"
+        >
+          Shop Now
+          <span className="iconify-[material-symbols--arrow-right-alt] transition-transform duration-500 ease-in-out group-hover:rotate-90"></span>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Hero from "../components/hero/Hero";
 
 function HomePage() {
   useEffect(() => {
@@ -15,9 +16,8 @@ function HomePage() {
 
   return (
     <>
-      <main className="mx-auto flex w-full max-w-7xl grow flex-col justify-center bg-amber-100 px-4 sm:px-12 xl:px-0">
-        <h1>Home Page</h1>
-        <p>Temporary view</p>
+      <main className="mx-auto flex w-full max-w-7xl grow flex-col justify-center sm:px-12 xl:px-0">
+        <Hero />
       </main>
     </>
   );
