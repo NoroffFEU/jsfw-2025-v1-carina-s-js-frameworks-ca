c3e2369dfe9aff666b75e5adc9aff7082ec4f881
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-04 14:15:34 +0100

    Product card mostly finished, needs star component

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -6,18 +6,28 @@ type Props = {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div>
-      <ol>
-        <li>{product.id}</li>
-        <li>{product.title}</li>
-        <li>{product.description}</li>
-        <li>{product.price}</li>
-        <li>{product.discountedPrice}</li>
-        <li>{product.image.url}</li>
-        <li>{product.image.alt}</li>
-        <li>{product.rating}</li>
-      </ol>
-    </div>
+    <article className="border-primary-ultra-light grid-cols-[minmax(0, 1fr)] grid cursor-pointer grid-rows-[max-content] rounded-sm border transition duration-300 ease-in-out sm:hover:scale-103 sm:hover:shadow-lg">
+      <img
+        src={product.image.url}
+        alt={product.image.alt}
+        className="h-95 w-full rounded-sm object-cover"
+      />
+      <div className="flex flex-col gap-6 p-4">
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-semibold">{product.title}</h3>
+          <p>{product.rating} - Stars coming soon...</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p className="text-2xl font-semibold">
+            {product.discountedPrice} NOK
+          </p>
+          <div className="text-gray-dark flex gap-2">
+            <p>Former Price:</p>
+            <s>{product.price} NOK</s>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
 
