12323e03576bc50b21b3b5ec4e6cfd37dda01141
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-04 14:26:43 +0100

    Added link to product card. Still missing stars, discount badge and favourites button.

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,4 +1,5 @@
 import type { Product } from "../../types/Product";
+import { Link } from "react-router-dom";
 
 type Props = {
   product: Product;
@@ -7,26 +8,28 @@ type Props = {
 const ProductCard = ({ product }: Props) => {
   return (
     <article className="border-primary-ultra-light grid-cols-[minmax(0, 1fr)] grid cursor-pointer grid-rows-[max-content] rounded-sm border transition duration-300 ease-in-out sm:hover:scale-103 sm:hover:shadow-lg">
-      <img
-        src={product.image.url}
-        alt={product.image.alt}
-        className="h-95 w-full rounded-sm object-cover"
-      />
-      <div className="flex flex-col gap-6 p-4">
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-semibold">{product.title}</h3>
-          <p>{product.rating} - Stars coming soon...</p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-2xl font-semibold">
-            {product.discountedPrice} NOK
-          </p>
-          <div className="text-gray-dark flex gap-2">
-            <p>Former Price:</p>
-            <s>{product.price} NOK</s>
+      <Link to={`/product/${product.id}`}>
+        <img
+          src={product.image.url}
+          alt={product.image.alt}
+          className="h-95 w-full rounded-sm object-cover"
+        />
+        <div className="flex flex-col gap-6 p-4">
+          <div className="flex flex-col gap-2">
+            <h3 className="text-lg font-semibold">{product.title}</h3>
+            <p>{product.rating} - Stars coming soon...</p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-2xl font-semibold">
+              {product.discountedPrice} NOK
+            </p>
+            <div className="text-gray-dark flex gap-2">
+              <p>Former Price:</p>
+              <s>{product.price} NOK</s>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </article>
   );
 };
