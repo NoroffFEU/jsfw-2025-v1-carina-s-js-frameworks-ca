e6247aa09a55723726a5b7f9975f1d74d5d17fb1
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-05 11:51:23 +0100

    Added btn-round class to styles.css

--- src/components/cart/CartButton.tsx
+++ src/components/cart/CartButton.tsx
@@ -4,11 +4,7 @@ type CartButtonProps = {
 
 const CartButton = ({ onClick }: CartButtonProps) => {
   return (
-    <button
-      onClick={onClick}
-      className="sm:hover:bg-secondary-ultra-light flex size-8 items-center justify-center rounded-full bg-white text-lg font-medium transition duration-500 ease-in-out sm:size-12"
-      aria-label="Cart"
-    >
+    <button onClick={onClick} className="btn-round" aria-label="Cart">
       <span className="iconify-[material-symbols--shopping-bag-outline]"></span>
     </button>
   );
--- src/components/favourites/FavouriteButton.tsx
+++ src/components/favourites/FavouriteButton.tsx
@@ -4,11 +4,7 @@ type FavouriteButtonProps = {
 
 const FavouriteButton = ({ onClick }: FavouriteButtonProps) => {
   return (
-    <button
-      onClick={onClick}
-      className="sm:hover:bg-secondary-ultra-light flex size-8 items-center justify-center rounded-full bg-white text-lg font-medium transition duration-500 ease-in-out sm:size-12"
-      aria-label="Favourites"
-    >
+    <button onClick={onClick} className="btn-round" aria-label="Favourites">
       <span className="iconify-[material-symbols--favorite-outline]"></span>
     </button>
   );
