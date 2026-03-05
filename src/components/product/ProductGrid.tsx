e6adac9bcc3f53a586a55745c47cb7b8ad2e81ac
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-05 10:21:29 +0100

    Added load more products with inifineQuery

--- src/components/product/ProductGrid.tsx
+++ src/components/product/ProductGrid.tsx
@@ -2,20 +2,26 @@ import ProductCard from "./ProductCard";
 import { useProducts } from "../../hooks/useProducts";
 import Sort from "../common/Sort";
 import { useProductFilters } from "../../hooks/useProductFilters";
+import Spinner from "../common/LoadingSpinner";
 
 const ProductGrid = () => {
   const { sort, sortOrder, handleSortChange } = useProductFilters();
 
-  const { data, isLoading, isError, error } = useProducts(
-    1,
-    12,
-    sort,
-    sortOrder,
-  );
+  const {
+    data,
+    isLoading,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useProducts(12, sort, sortOrder);
 
   if (isLoading) return <p>Loading...</p>;
 
   if (isError) return <p>{(error as Error).message}</p>;
+
+  const products = data?.pages.flatMap((page) => page.data) ?? [];
 
   return (
     <section className="px-4 py-8 sm:px-0" id="products-section">
@@ -25,14 +31,28 @@ const ProductGrid = () => {
       <div className="mt-2 flex justify-start sm:justify-end">
         <Sort onSortChange={handleSortChange} />
       </div>
-      <div className="grid gap-12 py-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {data?.data.map((product) => (
+      <div className="grid gap-12 py-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-      <div className="py-4 text-center">
-        <p>Scroll to load more coming soon...</p>
-      </div>
+      {hasNextPage && (
+        <div className="flex justify-center py-6">
+          <button
+            onClick={() => fetchNextPage()}
+            className="btn-primary flex items-center justify-center gap-2"
+          >
+            {isFetchingNextPage ? (
+              <>
+                <Spinner />
+                Loading...
+              </>
+            ) : (
+              "Load More"
+            )}
+          </button>
+        </div>
+      )}
     </section>
   );
 };
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,14 +1,22 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getAllProducts } from "../services/getProducts";
 
 export function useProducts(
-  page: number = 1,
-  limit: number = 12,
+  limit: number,
   sort?: string,
   sortOrder?: "asc" | "desc",
 ) {
-  return useQuery({
-    queryKey: ["products", page, limit, sort, sortOrder],
-    queryFn: () => getAllProducts(page, limit, sort, sortOrder),
+  return useInfiniteQuery({
+    queryKey: ["products", sort, sortOrder],
+
+    queryFn: ({ pageParam = 1 }) =>
+      getAllProducts(pageParam, limit, sort, sortOrder),
+
+    initialPageParam: 1,
+
+    getNextPageParam: (lastPage, allPages) => {
+      if (!lastPage.meta.nextPage) return undefined;
+      return allPages.length + 1;
+    },
   });
 }
