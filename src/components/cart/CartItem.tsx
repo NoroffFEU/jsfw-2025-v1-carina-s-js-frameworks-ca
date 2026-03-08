4e483e5b04abab3c8763d108eaa126aa06d81127
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-08 16:01:37 +0100

    Completed cart item styling

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -15,41 +15,48 @@ const CartItem = ({ product, quantity }: Props) => {
   const removeItem = useCartStore((state) => state.removeItem);
 
   return (
-    <article className="border-gray-light grid-cols-[minmax(0, 1fr)] grid cursor-pointer grid-rows-[max-content] rounded-sm border transition duration-300 ease-in-out sm:hover:scale-103 sm:hover:shadow-lg">
-      <Link to={`/product/${product.id}`}>
-        <div className="relative">
-          <DiscountBadge product={product} />
-          <img
-            src={product.image.url}
-            alt={product.image.alt}
-            className="aspect-3/4 w-full rounded-sm object-cover"
-          />
-        </div>
-        <QuantitySelector productId={product.id} quantity={quantity} />
-        <RemoveButton onClick={() => removeItem(product.id)} />
-        <div className="grid-cols-auto grid gap-6 p-4">
-          <div className="mt-auto flex flex-col gap-2">
-            <h3 className="font-semibold sm:text-lg">{product.title}</h3>
+    <article className="flex-col">
+      <div className="flex gap-4 py-4">
+        <Link to={`/product/${product.id}`}>
+          <div className="relative size-22 overflow-hidden rounded-sm">
+            <DiscountBadge product={product} size="small" />
+            <img
+              src={product.image.url}
+              alt={product.image.alt}
+              className="object-cover"
+            />
           </div>
-          <div className="flex flex-col gap-1 sm:gap-2">
-            {hasDiscount ? (
-              <>
-                <p className="text-lg font-semibold sm:text-2xl">
-                  {product.discountedPrice} NOK
-                </p>
-                <div className="text-gray-dark flex flex-col flex-wrap sm:gap-2 md:flex-row">
-                  <p>Former Price:</p>
-                  <s>{product.price} NOK</s>
-                </div>
-              </>
-            ) : (
-              <p className="text-lg font-semibold sm:text-2xl">
-                {product.price} NOK
-              </p>
-            )}
+        </Link>
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex flex-col">
+            <div className="flex items-start justify-between">
+              <h3 className="font-semibold">{product.title}</h3>
+              <RemoveButton onClick={() => removeItem(product.id)} />
+            </div>
+            <div className="mt-2">
+              <QuantitySelector productId={product.id} quantity={quantity} />
+            </div>
+
+            <div className="flex justify-end">
+              <div className="flex flex-col gap-1 text-right sm:gap-2">
+                {hasDiscount ? (
+                  <>
+                    <s className="text-gray-dark text-sm">
+                      {product.price} NOK
+                    </s>
+                    <p className="font-semibold">
+                      {product.discountedPrice} NOK
+                    </p>
+                  </>
+                ) : (
+                  <p className="font-semibold">{product.price} NOK</p>
+                )}
+              </div>
+            </div>
           </div>
         </div>
-      </Link>
+      </div>
+      <hr className="text-gray-medium" />
     </article>
   );
 };
