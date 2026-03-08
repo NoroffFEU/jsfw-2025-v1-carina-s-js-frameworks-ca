826460e8e3b918039dfc00ca9254f081c18d9bec
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-08 12:24:35 +0100

    Created cart modal - not complete

--- src/components/cart/CartModal.tsx
+++ src/components/cart/CartModal.tsx
@@ -4,47 +4,97 @@ import {
   DialogTitle,
   DialogBackdrop,
 } from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
+import CartItem from "./CartItem";
+import useCartStore from "../../store/cartStore";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
+  id: string;
 };
 
 function CartModal({ isOpen, onClose }: Props) {
+  const items = useCartStore((state) => state.items);
+  const total = useCartStore((state) => state.getTotal());
+  //const removeItem = useCartStore((state) => state.removeItem);
+  //const updateQuantity = useCartStore((state) => state.updateQuantity);
+
+  if (items.length === 0) {
+    return <p>Cart is empty.</p>;
+  }
+
   return (
-    <Dialog open={isOpen} onClose={onClose} className="relative z-50">
-      <DialogBackdrop className="fixed inset-0 bg-black/30" />
-      <div className="fixed inset-0 flex w-full items-center justify-end">
-        <DialogPanel className="h-full w-[90%] max-w-145 space-y-4 rounded-sm bg-white p-4 sm:p-8">
-          <div className="space-y-2">
-            <div className="flex justify-between">
-              <DialogTitle className="text-2xl font-bold">
-                Your Cart
-              </DialogTitle>
-              <button
-                className="btn-round flex items-center justify-center"
-                onClick={onClose}
-              >
-                <span className="iconify-[material-symbols--close]"></span>
-              </button>
-            </div>
-            <hr className="text-gray-medium" />
-          </div>
-          <div>
-            <p>This is an item</p>
-          </div>
-          <div>
-            <div className="flex justify-between font-semibold">
-              <p>Total</p>
-              <p>5 600 NOK</p>
-            </div>
-            <button className="btn-primary" onClick={onClose}>
-              One-Click Checkout
-            </button>
-          </div>
-        </DialogPanel>
-      </div>
-    </Dialog>
+    <AnimatePresence>
+      {isOpen && (
+        <Dialog
+          static
+          open={isOpen}
+          onClose={onClose}
+          className="relative z-50"
+        >
+          <DialogBackdrop
+            className="fixed inset-0 bg-black/30"
+            as={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          />
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.35, ease: "easeInOut" }}
+            className="fixed inset-0 flex w-full items-center justify-end overflow-hidden"
+          >
+            <DialogPanel className="h-full w-[90%] max-w-145 space-y-4 rounded-sm bg-white p-4 sm:p-8">
+              <div className="space-y-2">
+                <div className="flex justify-between">
+                  <DialogTitle className="text-2xl font-bold">
+                    Your Cart
+                  </DialogTitle>
+                  <button
+                    className="btn-round flex items-center justify-center"
+                    onClick={onClose}
+                  >
+                    <span className="iconify-[material-symbols--close]"></span>
+                  </button>
+                </div>
+                <hr className="text-gray-medium" />
+              </div>
+              <div>
+                {items.map((item) => (
+                  <CartItem key={item.product.id} product={item.product} />
+                ))}
+              </div>
+              <div className="bg-secondary-ultra-light border-secondary text-gray-dark flex flex-col gap-4 rounded-sm border p-4 md:text-lg">
+                <div className="flex items-center justify-between">
+                  <p>Shipping:</p>
+                  <div className="flex items-center gap-2">
+                    <span className="iconify-[material-symbols--check]"></span>
+                    <p>Free</p>
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <p>Amount:</p>
+                  <p>2 380 NOK</p>
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-8">
+                <div className="flex justify-between text-lg font-semibold md:text-2xl">
+                  <p>Total</p>
+                  <p>{total}</p>
+                </div>
+                <button className="btn-primary" onClick={onClose}>
+                  One-Click Checkout
+                </button>
+              </div>
+            </DialogPanel>
+          </motion.div>
+        </Dialog>
+      )}
+    </AnimatePresence>
   );
 }
 
