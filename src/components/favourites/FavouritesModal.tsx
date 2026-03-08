c0ff99033c5cd2f0ed5d6a4b2a20e2159850d222
Carina M. Pedersen <carina.m.pedersen@gmail.com>
2026-03-08 10:13:16 +0100

    Updated favourites modal - not complete.

--- src/components/favourites/FavouritesModal.tsx
+++ src/components/favourites/FavouritesModal.tsx
@@ -1,14 +1,64 @@
-type FavouritesModalProps = {
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogBackdrop,
+} from "@headlessui/react";
+import { AnimatePresence, motion } from "framer-motion";
+
+type Props = {
+  isOpen: boolean;
   onClose: () => void;
 };
 
-const FavouritesModal = ({ onClose }: FavouritesModalProps) => {
+function FavouritesModal({ isOpen, onClose }: Props) {
   return (
-    <dialog open className="rounded-sm p-4 shadow-sm">
-      <p>This is a modal</p>
-      <button onClick={onClose}>Close</button>
-    </dialog>
+    <AnimatePresence>
+      {isOpen && (
+        <Dialog
+          static
+          open={isOpen}
+          onClose={onClose}
+          className="relative z-50"
+        >
+          <DialogBackdrop
+            className="fixed inset-0 bg-black/30"
+            as={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          />
+          <motion.div
+            initial={{ x: "100%" }}
+            animate={{ x: 0 }}
+            exit={{ x: "100%" }}
+            transition={{ duration: 0.35, ease: "easeInOut" }}
+            className="fixed inset-0 flex w-full items-center justify-end overflow-hidden"
+          >
+            <DialogPanel className="h-full w-[90%] max-w-145 space-y-4 rounded-sm bg-white p-4 sm:p-8">
+              <div className="space-y-2">
+                <div className="flex justify-between">
+                  <DialogTitle className="text-2xl font-bold">
+                    Your Favourites
+                  </DialogTitle>
+                  <button
+                    className="btn-round flex items-center justify-center"
+                    onClick={onClose}
+                  >
+                    <span className="iconify-[material-symbols--close]"></span>
+                  </button>
+                </div>
+                <hr className="text-gray-medium" />
+              </div>
+              <div>
+                <p>This is a favourite item</p>
+              </div>
+            </DialogPanel>
+          </motion.div>
+        </Dialog>
+      )}
+    </AnimatePresence>
   );
-};
+}
 
 export default FavouritesModal;
